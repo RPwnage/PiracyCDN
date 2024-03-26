45f68bdf96ed235730b444af11a1a56e5c50157f
root <root@Simon>
2024-03-26 14:08:12 +0100

    megakino deeplink requests are done threaded now

--- src/modules/megakino.py
+++ src/modules/megakino.py
@@ -1,13 +1,22 @@
 import requests
 from ..Movie import Movie
 from bs4 import BeautifulSoup
+from multiprocessing.pool import ThreadPool
+
 
 SITE_IDENTIFIER = "megakino"
 SITE_NAME = "Megakino"
 
 
-def fetchDataFromDeeplink(url: str) -> dict:
-    pass
+def __fetchDataFromDeeplink(url: str) -> dict:
+    dRes = requests.get(str(url))
+    dSoup = BeautifulSoup(dRes.text, "html.parser")
+    dDescription = str((dSoup.find("div", class_="page__text")).text)
+    if dSoup.find("span", itemprop="dateCreated") is not None:
+        dDateCreated = str((dSoup.find("span", itemprop="dateCreated")).text)
+    dOriginalTitle = str((dSoup.find("div", class_="pmovie__original-title")).text)[1:]
+
+    return [dOriginalTitle, dDescription]
 
 
 def search(title: str) -> list:
@@ -17,45 +26,43 @@ def search(title: str) -> list:
     )
     soup = BeautifulSoup(res.text, "html.parser")
     responseElements = soup.findAll("a", class_="poster")
-    response = []
+    movieDeeplinks = list()
+    movieData = list()
+    movieDeeplinkData = list()
+    retObj = list()
+    pool = ThreadPool(processes=int(len(responseElements) + 1))
+    async_result = list()
     for responseElement in responseElements:
         elemSoup = BeautifulSoup(str(responseElement), "html.parser")
         title = str((elemSoup.find("h3", class_="poster__title")).text)
         shortDescription = str((elemSoup.find("div", class_="poster__text")).text)
         poster = str("https://megakino.co/" + (elemSoup.find("img"))["data-src"])
-        movieDeeplink = (elemSoup.find("a", class_="poster"))["href"]
+        movieData.append([title, shortDescription, poster])
+        movieDeeplinks.append(
+            elemSoup.find("div", class_="thumbnail").find("a")["href"]
+        )
 
-        # Fetch data from deeplink
-        dRes = requests.get(str(movieDeeplink))
-        dSoup = BeautifulSoup(dRes.text, "html.parser")
-        dDescription = str((dSoup.find("div", class_="page__text")).text)
-        if dSoup.find("span", itemprop="dateCreated") is not None:
-            dDateCreated = str((dSoup.find("span", itemprop="dateCreated")).text)
-        dCountryOfOrigin = str((dSoup.find("span", itemprop="countryOfOrigin")).text)
-        dRecommendedAge = str((dSoup.find("div", class_="pmovie__age")).text)
-        dOriginalTitle = str((dSoup.find("div", class_="pmovie__original-title")).text)[
-            1:
-        ]
-        dMediaType = str((dSoup.find("div", class_="pmovie__genres")).text).split(" ")[
-            0
-        ]
-        if "Serien" in dMediaType:
-            dMediaType = "series"
-        else:
-            dMediaType = "movies"
+    for movieDeeplink in movieDeeplinks:
+        async_result.append(
+            pool.apply_async(__fetchDataFromDeeplink, (str(movieDeeplink),))
+        )
 
-        response.append(
+    for index, result in enumerate(async_result):
+        movieDeeplinkData.append(result.get())
+
+    for index, movie in enumerate(movieData):
+        retObj.append(
             Movie(
-                title,
-                originalTitle=dOriginalTitle,
-                shortDescription=shortDescription,
-                description=dDescription,
-                poster=poster,
+                title=movie[0],
+                originalTitle=movieDeeplinkData[index][0],
+                shortDescription=movie[1],
+                description=movieDeeplinkData[index][1],
+                poster=movie[2],
                 provider=SITE_NAME,
             )
         )
 
-    return response
+    return retObj
 
 
 def showEpisodes(titleId: int) -> list:
